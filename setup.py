055846e1a822f40d057bc97191fd3683f4a6f22d
Jim Fulton <jim@jimfulton.info>
2017-06-12 13:25:28 -0400

    Back to development: 0.1.2

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 name = 'j1m.ravenzconfig'
-version = '0.1.1'
+version = '0.1.2.dev0'
 
 install_requires = ['setuptools', 'raven', 'ZConfig']
 extras_require = dict(test=['manuel', 'mock', 'zope.testing'])
